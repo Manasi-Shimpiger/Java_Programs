a89b53499c745f460bca6ee6d14eaf0359fe7cd6
Manasi-Shimpiger <manasishimpiger@gmail.com>
2022-08-31 12:26:21 +0530

    Eleventh commit

--- src/Searching_algorithm/BinarySearch.java
+++ src/Searching_algorithm/BinarySearch.java
@@ -2,23 +2,38 @@ package Searching_algorithm;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int a[]={-2,0,4,6,8,0,20,25,29};
-        int ans=search(a,25);
+        int a[]={-2,0,4,6,8};
+        int ans=search(a,4);
         System.out.println(ans);
     }
     public static int search(int a[],int target){
         int start=0;
-        int end=a.length;
-        while(start<=end){
-            int mid=start+(end-start)/2;   //mid value might exceed limit,so using (end-start)
-            if(target<a[mid]){
-                end=mid-1;
-            } else if (target>a[mid]) {
-                start=mid+1;
-            }else
-                return mid;
-        }
+        int end=a.length-1;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;//mid value might exceed limit,so using (end-start)
+
+                if(target==a[mid]){
+                    return mid;
+                }
+                //if array is sorted in ascending order
+                if(a[start]<a[end]){
+                    if (target < a[mid]) {
+                        end = mid - 1;
+                    } else if (target > a[mid]) {
+                        start = mid + 1;
+                    }
+                }
+                //if array is sorted in descending order
+                else if (a[start]>a[end]) {
+                    if (target < a[mid]) {
+                        start = mid + 1;
+                    } else if (target > a[mid]) {
+                        end = mid - 1;
+                    }
+                }
+            }
         return -1; //if element is not found return -1
     }
+
 }
 
